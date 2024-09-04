e4e8466dabe5cea7646ce371ae04b0b1f7071f6c
Eusebiusclyde <barracliffeclyde@gmail.com>
2024-09-05 03:50:59 +0530

    Update dependencies and add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='my-streamlit-app',
+    version='1.0',
+    install_requires=[
+        'streamlit==1.38.0',
+        'pandas==2.0.2',
+        'numpy==1.24.3',
+    ],
+)
